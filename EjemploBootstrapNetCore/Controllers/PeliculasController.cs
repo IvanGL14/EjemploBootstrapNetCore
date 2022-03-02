5c7ff3e3f3b8801c7c1cfc16a540db58af018d77
Iván Gonzalo Lucas <ivan.gonzalo@tajamar365.com>
2022-03-02 20:58:08 +0100

    Add project files.

---
+++ EjemploBootstrapNetCore/Controllers/PeliculasController.cs
@@ -0,0 +1,55 @@
+﻿using EjemploBootstrapNetCore.Models;
+using EjemploBootstrapNetCore.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjemploBootstrapNetCore.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private RepositoryPeliculas repo;
+
+        public PeliculasController(RepositoryPeliculas repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            List<Pelicula> peliculas = this.repo.GetPeliculas();
+            return View(peliculas);
+        }
+
+        public IActionResult _InfoPeliculaPartial(int idpelicula)
+        {
+            Pelicula peli = this.repo.GetPeliculaDetails(idpelicula);
+            return PartialView("_InfoPeliculaPartial", peli);
+        }
+
+        public IActionResult NormalForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NormalForm(string Titulo, int Duracion, string Sinopsis, string Genero, string Trailer, string Imagen, int Valoracion)
+        {
+            this.repo.InsertarPelicula(Titulo, Duracion, Sinopsis, Genero, Trailer, Imagen, Valoracion);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ModalForm()
+        {
+            return PartialView("_ModalForm");
+        }
+
+        [HttpPost]
+        public IActionResult ModalForm(string Titulo, int Duracion, string Sinopsis, string Genero, string Trailer, string Imagen, int Valoracion)
+        {
+            this.repo.InsertarPelicula(Titulo, Duracion, Sinopsis, Genero, Trailer, Imagen, Valoracion);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EjemploBootstrapNetCore/Repositories/RepositoryPeliculas.cs
@@ -0,0 +1,53 @@
+﻿using EjemploBootstrapNetCore.Data;
+using EjemploBootstrapNetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjemploBootstrapNetCore.Repositories
+{ 
+    public class RepositoryPeliculas
+    {
+        private PeliculasContext context;
+
+        public RepositoryPeliculas(PeliculasContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Pelicula> GetPeliculas()
+        {
+            return this.context.Peliculas.ToList();
+        }
+
+        public Pelicula GetPeliculaDetails(int idpelicula)
+        {
+            return this.GetPeliculas().FirstOrDefault(z => z.IdPelicula == idpelicula);
+        }
+
+        public int GetMaxIdPeli()
+        {
+            var max = (from datos in this.context.Peliculas
+                       select datos.IdPelicula).Max();
+
+            return max + 1;
+        }
+
+        public void InsertarPelicula(string Titulo, int Duracion, string Sinopsis, string Genero, string Trailer, string Imagen, int Valoracion)
+        {
+            Pelicula peli = new Pelicula();
+            peli.IdPelicula = this.GetMaxIdPeli();
+            peli.Titulo = Titulo;
+            peli.Duracion = Duracion;
+            peli.Sinopsis = Sinopsis;
+            peli.Genero = Genero;
+            peli.Trailer = Trailer;
+            peli.Imagen = Imagen;
+            peli.Valoracion = Valoracion;
+
+            this.context.Peliculas.Add(peli);
+            this.context.SaveChanges();
+        }
+    }
+}
